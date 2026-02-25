e752f35d97effbc1f25d0d83af7edf07ba750586
Kishan Kumar Gautam <kishan143ch@gmail.com>
2026-02-25 15:38:03 +0530

    Stop tracking dist and node_modules

--- backend/dist/routes/featureFlag.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const featureFlag_controller_1 = require("../controllers/featureFlag.controller");
-const router = (0, express_1.Router)();
-router.get("/", featureFlag_controller_1.getFeatureFlags);
-router.put("/", featureFlag_controller_1.updateFeatureFlags);
-exports.default = router;
--- backend/dist/types/pricing.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * Unified pricing types for backend, admin, and frontend
- * Single source of truth for all price calculation interfaces
- */
-Object.defineProperty(exports, "__esModule", { value: true });
